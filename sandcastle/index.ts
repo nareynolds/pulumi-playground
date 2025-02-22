5da0b7f8022bea29a8f8dc6c34ffa638acaa162c
Nathaniel Reynolds <nathaniel.reynolds@gmail.com>
2025-02-22 14:23:51 -0500

    2nd bucket

--- sandcastle/index.ts
+++ sandcastle/index.ts
@@ -5,5 +5,8 @@ import * as awsx from "@pulumi/awsx";
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.BucketV2("my-bucket");
 
+// Create an AWS resource (S3 Bucket)
+const bucket = new aws.s3.BucketV2("my-2nd-bucket");
+
 // Export the name of the bucket
 export const bucketName = bucket.id;
