044714af50574288c45a4d1edf3c18693d815a53
Nathaniel Reynolds <nathaniel.reynolds@gmail.com>
2025-02-28 15:56:16 -0500

    remove resources

--- sandcastle/index.ts
+++ sandcastle/index.ts
@@ -2,11 +2,11 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+// // Create an AWS resource (S3 Bucket)
+// const bucket = new aws.s3.BucketV2("my-bucket");
 
-// Create an AWS resource (S3 Bucket)
-const bucket2 = new aws.s3.BucketV2("my-2nd-bucket");
+// // Create an AWS resource (S3 Bucket)
+// const bucket2 = new aws.s3.BucketV2("my-2nd-bucket");
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// // Export the name of the bucket
+// export const bucketName = bucket.id;
